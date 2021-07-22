3e734c1a65738b095a3e52b3b4cf3cbc3793e8d9
doger123 <73554572+doger123@users.noreply.github.com>
2021-07-22 11:03:48 -0400

    clean

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var clicks = 0;
 var timeSeconds = 0;
 var cps = 0;
 var time = new Date();
-var currentTime = time.getTime()
-var howLong = 5;    // Change when buttons for setting time work
+var currentTime = time.getTime();
+var howLong = 5; // Change when buttons for setting time work
 const cpsDisplay = document.querySelector(".cpsdisplay");
 const timeElapsed = document.querySelector(".timeremaining");
 const totalClicks = document.querySelector(".totalclicks");
@@ -24,77 +24,88 @@ const sixtySec = document.querySelector("#sixtySeconds");
 
 var selectedTime = fiveSec;
 
-
 // Event listeners
 button.addEventListener("click", click);
 
-oneSec.addEventListener("click", function(){return timeChange(1)});
-fiveSec.addEventListener("click", function(){return timeChange(5)});
-tenSec.addEventListener("click", function(){return timeChange(10)});
-fifteenSec.addEventListener("click", function(){return timeChange(15)});
-twentySec.addEventListener("click", function(){return timeChange(20)});
-thirtySec.addEventListener("click", function(){return timeChange(30)});
-sixtySec.addEventListener("click", function(){return timeChange(60)});
+oneSec.addEventListener("click", function () {
+  return timeChange(1);
+});
+fiveSec.addEventListener("click", function () {
+  return timeChange(5);
+});
+tenSec.addEventListener("click", function () {
+  return timeChange(10);
+});
+fifteenSec.addEventListener("click", function () {
+  return timeChange(15);
+});
+twentySec.addEventListener("click", function () {
+  return timeChange(20);
+});
+thirtySec.addEventListener("click", function () {
+  return timeChange(30);
+});
+sixtySec.addEventListener("click", function () {
+  return timeChange(60);
+});
 
 function click() {
+  if (done === false) {
+    if (clicks === 0) {
+      textInButton.style.opacity = "0";
+      var thingTime = Date.now();
+      startingTime = thingTime;
+      updateTime();
+    }
 
-     if (done === false)  {
-          if (clicks === 0) {
-               textInButton.style.opacity = "0";
-               var thingTime = Date.now();
-               startingTime = thingTime;
-               updateTime()
-          }
-
-          clicks++;
-          totalClicks.innerHTML = clicks;
-     }
+    clicks++;
+    totalClicks.innerHTML = clicks;
+  }
 }
 
 function updateTime() {
-     var now = Date.now();
-     timeSeconds = (now - startingTime) / 1000;
-     timeSeconds = Math.round(timeSeconds * 100) / 100;
-     timeElapsed.innerHTML = timeSeconds.toFixed(2);
-     cps = Math.round((clicks / timeSeconds) * 100) / 100;
-     cpsDisplay.innerHTML = cps.toFixed(1);
-     if (timeSeconds > howLong) {
-          timeElapsed.innerHTML = howLong;
-          done = true;
-     }
-     else {
-          setTimeout(updateTime, 10);
-     }
+  var now = Date.now();
+  timeSeconds = (now - startingTime) / 1000;
+  timeSeconds = Math.round(timeSeconds * 100) / 100;
+  timeElapsed.innerHTML = timeSeconds.toFixed(2);
+  cps = Math.round((clicks / timeSeconds) * 100) / 100;
+  cpsDisplay.innerHTML = cps.toFixed(1);
+  if (timeSeconds > howLong) {
+    timeElapsed.innerHTML = howLong;
+    done = true;
+  } else {
+    setTimeout(updateTime, 10);
+  }
 }
 
 function timeChange(num) {
-     if (clicks === 0) {
-          howLong = num;
-          selectedTime.style.backgroundColor = "rgb(69, 117, 138)";
-          switch (num) {
-               case 1:
-                    selectedTime = oneSec;
-                    break;
-               case 5:
-                    selectedTime = fiveSec;
-                    break;
-               case 10:
-                    selectedTime = tenSec;
-                    break;
-               case 15:
-                    selectedTime = fifteenSec;
-                    break;
-               case 20:
-                    selectedTime = twentySec;
-                    break;
-               case 30:
-                    selectedTime = thirtySec;
-                    break;
-               case 60:
-                    selectedTime = sixtySec;
-                    break;
-          }
+  if (clicks === 0) {
+    howLong = num;
+    selectedTime.style.backgroundColor = "rgb(69, 117, 138)";
+    switch (num) {
+      case 1:
+        selectedTime = oneSec;
+        break;
+      case 5:
+        selectedTime = fiveSec;
+        break;
+      case 10:
+        selectedTime = tenSec;
+        break;
+      case 15:
+        selectedTime = fifteenSec;
+        break;
+      case 20:
+        selectedTime = twentySec;
+        break;
+      case 30:
+        selectedTime = thirtySec;
+        break;
+      case 60:
+        selectedTime = sixtySec;
+        break;
+    }
 
-          selectedTime.style.backgroundColor = "rgb(139, 67, 67)";
-     }
+    selectedTime.style.backgroundColor = "rgb(139, 67, 67)";
+  }
 }
